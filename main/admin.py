5cf05580fa443dff0c75407b32415a75558bbe31
Pana34 <slaven.pnc@gmail.com>
2024-12-12 12:35:01 +0100

    Delete main directory

--- main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from main.models import *
-# Register your models here.
-
-model_list = [Budzet, Prihod, Rashod, Tag]
-admin.site.register(model_list)
--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('budzet/', views.budzet_list, name='budzet_list'),
-    path('prihod/', views.prihod_list, name='prihod_list'),
-    path('rashod/', views.rashod_list, name='rashod_list'),
-    path('tag/', views.tag_list, name='tag_list'),
-]
